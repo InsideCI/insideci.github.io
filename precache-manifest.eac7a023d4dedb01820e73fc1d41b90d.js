ff3e1f07fee0063a0f2fd2cac8da3950b9039581
Cleanderson Lins <cleandersonlins@gmail.com>
2020-04-09 01:18:22 -0300

    Updates

---
+++ precache-manifest.eac7a023d4dedb01820e73fc1d41b90d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b47b867fa9f1ea9b0717da09209f9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "004e9b568d0340b11209",
+    "url": "/static/css/main.6a2902eb.chunk.css"
+  },
+  {
+    "revision": "241604d2fea02eebf6b0",
+    "url": "/static/js/2.f1bf5818.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.f1bf5818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004e9b568d0340b11209",
+    "url": "/static/js/main.b6ad5d70.chunk.js"
+  },
+  {
+    "revision": "b705f15ea78a80fb2cc9",
+    "url": "/static/js/runtime-main.ef361466.js"
+  }
+]);
