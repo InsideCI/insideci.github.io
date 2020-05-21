9ff831a8614ceb9c21ae932ec3bb7ae37e8af15d
Cleanderson Lins <cleandersonlins@gmail.com>
2020-05-20 23:19:06 -0300

    Changes in overwall styles for a better mobile first approach

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -3,12 +3,10 @@ import './Start.scss';
 import React, { useState } from 'react';
 import { animated, config, useTransition } from 'react-spring';
 
-import Button from 'components/Button';
-import { Form } from '@unform/web';
-import Input from 'components/Input';
-import { StartAPI } from './store';
-import { Student } from 'store/types/models';
-import { SubmitHandler } from '@unform/core';
+import bottomLeft from '../../assets/images/landing/bottom-left.svg';
+import bottomRight from '../../assets/images/landing/bottom-right.svg';
+import topLeft from '../../assets/images/landing/top-left.svg';
+import topRight from '../../assets/images/landing/top-right.svg';
 
 interface Form {
   id: number;
@@ -17,41 +15,28 @@ interface Form {
 type Props = React.HTMLProps<HTMLDivElement>;
 
 const Start: React.FC<Props> = () => {
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState({});
-  const [user, setUser] = useState<Student>();
   const [section, setSection] = useState(true);
 
-  const onSubmit: SubmitHandler<Form> = (formData) => {
-    StartAPI.get(formData.id)
-      .then((response) => {
-        setLoading(true);
-        setUser(response.data);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-    console.log({ loading, user, error });
-  };
-
-  const registrationForm = () => (
-    <>
-      <Form onSubmit={onSubmit} className={'credentials'}>
-        <Input
-          name={'id'}
-          type={'number'}
-          placeholder={'registration'}
-          small
-          full
-        />
-        <Button text={'ENTER'} type={'submit'} light />
-        <Button text={'enter as a guest'} type={'submit'} light faded />
-      </Form>
-      <Button text={'github'} onClick={() => setSection(!section)} light />
-    </>
+  const landingBackground = () => (
+    <section className={'background'}>
+      <img className={'tl'} src={topLeft} alt="topRight" />
+      <img className={'tr'} src={topRight} alt="topRight" />
+      <img className={'bl'} src={bottomLeft} alt="topRight" />
+      <img className={'br'} src={bottomRight} alt="topRight" />
+    </section>
   );
 
-  const presensation = () => <>comé que ta por ai papai</>;
+  const greetings = () => (
+    <div className="greetings">
+      <header>
+        <span>Bem-vindo ao </span>
+        <h1>CInside</h1>
+      </header>
+      <footer onClick={() => setSection(!section)}>Entrar</footer>
+    </div>
+  );
+
+  const credentials = () => <>comé que ta por ai papai</>;
 
   const sectionTransition = useTransition(section, null, {
     config: config.gentle,
@@ -62,15 +47,15 @@ const Start: React.FC<Props> = () => {
 
   return (
     <div className={'landing'}>
-      <header>CInside</header>
+      {landingBackground()}
       {sectionTransition.map(({ item, key, props }) => {
         return item ? (
           <animated.div key={key} style={props}>
-            {registrationForm()}
+            {greetings()}
           </animated.div>
         ) : (
           <animated.div key={key} style={{ ...props, color: 'white' }}>
-            {presensation()}
+            {credentials()}
           </animated.div>
         );
       })}
