eb53059f11f2c156775edb1a5670e708f1015c04
Cleanderson Lins <cleandersonlins@gmail.com>
2020-05-25 23:02:17 -0300

    Updates

---
+++ precache-manifest.b816707a49078ee0464cec807342db45.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868daa799df754bf83ce3d4c91df6ba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ee490c39b038151e417",
+    "url": "/static/css/main.3c816ed7.chunk.css"
+  },
+  {
+    "revision": "fc03b9d06b31002a0f3c",
+    "url": "/static/js/2.996cdf28.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/static/js/2.996cdf28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee490c39b038151e417",
+    "url": "/static/js/main.4b51660c.chunk.js"
+  },
+  {
+    "revision": "dbdc5fd311c084f04c37",
+    "url": "/static/js/runtime-main.900d594e.js"
+  },
+  {
+    "revision": "f8b68715e7b6017ed95a100e2dacf5be",
+    "url": "/static/media/bottom-left.f8b68715.svg"
+  },
+  {
+    "revision": "fe7757749bc6c4b3353a5412068524f8",
+    "url": "/static/media/bottom-right.fe775774.svg"
+  },
+  {
+    "revision": "26c07d1a1d587ef3fe62b76d37bd4ab6",
+    "url": "/static/media/top-left.26c07d1a.svg"
+  },
+  {
+    "revision": "74681e9adb6218584c3fc8f198c71036",
+    "url": "/static/media/top-right.74681e9a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48887fe55a81d11b8ff83ebaef0648a4.js"
+  "/precache-manifest.b816707a49078ee0464cec807342db45.js"
 );
 
 self.addEventListener('message', (event) => {
